3929dbd474eea2a129bd18d221ec77bd41e22a1d
Kouhei-Yamamoto-JP <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-22 13:24:17 +0000

    Sushi: flip tuna mayo; strip magenta fringe on gunkan icons

--- sushi/game.js
+++ sushi/game.js
@@ -432,7 +432,7 @@
   }
 
   const ICON_BASE = "assets/icons/";
-  const ICON_VER = "20260922g";
+  const ICON_VER = "20260922h";
   const ING_ICON_FILE = {
     shari: "shari.png",
     nori: "nori.png",
